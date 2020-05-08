4f69f1320b93719db2baa8374687a6cf72cd30e1
mbsantiago <santiago.mbal@gmail.com>
2020-05-08 14:43:02 -0500

    Offset and limit error fix

--- yuntu/collection/base.py
+++ yuntu/collection/base.py
@@ -64,7 +64,7 @@ class Collection:
             limit=None,
             offset=None,
             with_annotations=False):
-        query_slice = slice(offset, limit)
+        query_slice = slice(offset, offset + limit)
         recordings = self.recordings(query=query)[query_slice]
 
         records = []
@@ -83,7 +83,7 @@ class Collection:
         return pd.DataFrame(records)
 
     def get_annotation_dataframe(self, query=None, limit=None, offset=None):
-        query_slice = slice(offset, limit)
+        query_slice = slice(offset, offset + limit)
         annotations = self.annotations(query=query)[query_slice]
 
         records = []
