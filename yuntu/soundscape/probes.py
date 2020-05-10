aef01db12b62e877d258388d0d7c58ddd790f388
Everardo <erobredo@conabio.gob.mx>
2020-05-10 11:24:23 -0500

    Working probe

--- yuntu/soundscape/probes.py
+++ yuntu/soundscape/probes.py
@@ -1,8 +1,10 @@
 """Classes for audio probes."""
 from abc import ABC
 from abc import abstractmethod
-# from scipy.signal import correlate2d
-# from skimage.feature import peak_local_max
+import numpy as np
+from skimage.feature import peak_local_max, match_template
+from shapely.ops import unary_union
+from yuntu.core.geometry.base import BBox, Polygon, FrequencyInterval
 
 
 class Probe(ABC):
@@ -14,64 +16,148 @@ class Probe(ABC):
     """
 
     @abstractmethod
-    def apply(self, media):
+    def apply(self, target, **kwargs):
         """Apply probe and return matches."""
 
-    def __call__(self, media):
-        return self.apply(media)
+    def __call__(self, target, **kwargs):
+        return self.apply(target, **kwargs)
 
 
 class TemplateProbe(Probe, ABC):
     """A probe that uses a template to find similar matches."""
-    _template = None
+
+    @abstractmethod
+    @property
+    def template(self):
+        """Return probe's template."""
+
+    @abstractmethod
+    def compare(self, target):
+        """Compare target with self's template."""
+
+
+class CrossCorrelationProbe(TemplateProbe):
+    """A probe that uses cross correaltion to match inputs with templates."""
+    name = "Correlation probe"
+
+    def __init__(self, mold):
+        if not isinstance(mold, (tuple, list)):
+            raise ValueError("Argument 'mold' must be a list of "
+                             "time/frequency media.")
+        self._template = []
+        self._frequency_interval = None
+        self.set_template(mold)
 
     @property
     def template(self):
         """Return probe's template."""
         return self._template
 
-    @abstractmethod
-    def compare(self, template):
-        """Compare input template with self's template."""
+    def set_template(self, mold):
+        for m in mold:
+            self.add_template(m)
 
-    @abstractmethod
-    def to_template(self, mold):
-        """Use mold to produce a template."""
+    def add_template(self, mold):
+        if self._template is None:
+            self._template = []
+        self._template.append(mold.array)
+        self._extend_interval_with(mold)
+
+    def _extend_interval_with(self, mold):
+        freqs = mold.frequencies
+        if self._frequency_interval is None:
+            self._frequency_interval = FrequencyInterval(min_freq=freqs[0],
+                                                         max_freq=freqs[-1])
+        else:
+            min_freq = self._frequency_interval.min_freq
+            max_freq = self._frequency_interval.max_freq
+            if freqs[0] < min_freq:
+                min_freq = freqs[0]
+            if freqs[1] > max_freq:
+                max_freq = freqs[-1]
+            self._frequency_interval = FrequencyInterval(min_freq=min_freq,
+                                                         max_freq=max_freq)
+
+    def compare(self, target):
+        return np.array([match_template(target.array, templ, pad_input=True)
+                        for templ in self.template])
 
 
-# class CorrelationProbe(TemplateProbe):
-#     """A probe that uses cross correaltion to find matches."""
-#     name = "Correlation probe"
-#
-#     def __init__(self, mold, name=None):
-#         if not isinstance(mold, Media):
-#             raise TypeError("Argument 'media' must be a Media object.")
-#         if name is not None:
-#             self.name = name
-#         self.name = name
-#         self._template = self.to_template(mold)
-#
-#     def to_template(self, mold):
-#         return mold
-#
-#     def compare(self, media):
-#         return correlate2d(media.array,
-#                            self.template,
-#                            boundary="symm",
-#                            mode="same")
-#
-#     def apply(self, media, thresh=0.0):
-#         min_distance = min(*self.shape)
-#         curr_d = 0
-#         next_d = 1
-#         if self.shape[1] == min_distance:
-#             curr_d = 1
-#             next_d = 0
-#         corr = self.compare(media)
-#         all_peaks = peak_local_max(corr,
-#                                    min_distance=min_distance,
-#                                    threshold_abs=thresh)
-#         return all_peaks
-#
-#     def shape(self):
-#         return self.template.shape
+    def apply(self, target, thresh=0.5, method='mean',
+              peak_distance=10, limit_freqs=True):
+        if not isinstance(limit_freqs, FrequencyInterval):
+            if limit_freqs:
+                limit_freqs = self.frequency_interval
+
+        min_distancex = self.shape[0]
+        min_distancey = self.shape[1]
+
+        corr = self.compare(target)
+        if method == 'max':
+            corr = np.amax(corr, axis=0)
+        else:
+            corr = np.mean(corr, axis=0)
+        all_peaks = peak_local_max(corr,
+                                   min_distance=peak_distance,
+                                   threshold_abs=thresh)
+
+        boxes = []
+        for x, y in all_peaks:
+            l1 = max(0, x - int(round(min_distancex/2)))
+            l2 = min(l1 + min_distancex, corr.shape[0])-1
+            if l2 - l1 > 1:
+                min_freq = target.frequencies[l1]
+                max_freq = target.frequencies[l2]
+                if limit_freqs:
+                    min_freq = max(min_freq, limit_freqs.min_freq)
+                    max_freq = min(max_freq, limit_freqs.max_freq)
+                m1 = max(0, y - int(round(min_distancey/2)))
+                m2 = min(m1 + min_distancey, corr.shape[1]-1)
+                if m2 - m1 > 1:
+                    start_time = target.times[m1]
+                    end_time = target.times[m2]
+                    new_box = BBox(start_time, end_time,
+                                   min_freq, max_freq)
+                    found = False
+                    for ind, box in enumerate(boxes):
+                        if new_box.intersects(box):
+                            disolved = unary_union([new_box.geometry,
+                                                    box.geometry])
+                            boxes[ind] = Polygon(geometry=disolved)
+                            found = True
+                            break
+                    if not found:
+                        boxes.append(BBox(start_time, end_time,
+                                          min_freq, max_freq))
+
+        output = []
+        for box in boxes:
+            corr_values = corr[target.to_mask(geometry=box)]
+            peak_corr = np.amax(corr_values)
+            result = {
+                "peak_corr": peak_corr,
+                "geometry": box
+            }
+            output.append(result)
+        return output
+
+    @property
+    def shape(self):
+        """Return probe's shape.
+
+        The shape of a probe is the shape of a ndarray that covers all
+        molds in template.
+        """
+        return np.amax(np.array([x.shape for x in self.template]), axis=0)
+
+    @property
+    def frequency_interval(self):
+        """Return frequency interval of probe."""
+        return self._frequency_interval
+
+
+def probe(ptype="cross_correlation", **kwargs):
+    """Create probe of type 'ptype'."""
+    if ptype == "cross_correlation":
+        return CrossCorrelationProbe(**kwargs)
+    raise NotImplementedError(f"Probe type {ptype} not found.")
