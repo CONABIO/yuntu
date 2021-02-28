e396f4b0bc53be67f3bf2392eec242a858f5237c
Everardo <erobredo@conabio.gob.mx>
2021-02-28 05:42:25 -0600

    Fix string sql literal

--- yuntu/core/database/mixins/utils.py
+++ yuntu/core/database/mixins/utils.py
@@ -16,7 +16,9 @@ def pg_drop_db(config, admin_user="postgres", admin_password="postgres",
     try:
         conn.set_isolation_level(ISOLATION_LEVEL_AUTOCOMMIT)
         cursor = conn.cursor()
-        cursor.execute("""SELECT pg_terminate_backend(pid) FROM pg_stat_activity WHERE datname=%s""", (database,))
+        cursor.execute(sql.SQL(
+         "SELECT pg_terminate_backend(pid) FROM pg_stat_activity WHERE datname={}"
+        ).format(sql.Literal(database)))
         cursor.execute(sql.SQL(
          "DROP DATABASE {}"
         ).format(sql.Identifier(database)))
@@ -57,33 +59,5 @@ def pg_create_db(config, admin_user="postgres", admin_password="postgres",
     cursor.execute(sql.SQL(
         "GRANT ALL PRIVILEGES ON DATABASE {} TO %s"
     ).format(sql.Identifier(database)), [AsIs(username)])
-
-    conn.close()
-
-def pg_drop_db(config, admin_user="postgres", admin_password="postgres",
-                     admin_db="postgres"):
-    database = config["database"]
-    username = config["user"]
-    password = config["password"]
-    host = config["host"]
-    port = config["port"]
-
-    conn = connect(dbname=admin_db, user=admin_user, password=admin_password,
-                   host=host, port=port)
-    # try:
-    conn.set_isolation_level(ISOLATION_LEVEL_AUTOCOMMIT)
-    cursor = conn.cursor()
-    cursor.execute(sql.SQL(
-     "SELECT pg_terminate_backend(pid) " + \
-     "FROM pg_stat_activity WHERE datname={}"
-    ).format(sql.Identifier(database)))
-    cursor.execute(sql.SQL(
-     "DROP DATABASE {}"
-    ).format(sql.Identifier(database)))
-    print(f"Database '{database}' droped!")
-
-    # except:
-    #     print(f"Database '{database}' not found. Nothing droped.")
-
 
     conn.close()
