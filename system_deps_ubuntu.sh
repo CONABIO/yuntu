6ec2137868c3fa64a48ee1466fed8068d273d444
mbsantiago <santiago.mbal@gmail.com>
2021-03-30 10:33:50 +0100

    moved version into main package

--- system_deps_ubuntu.sh
+++ system_deps_ubuntu.sh
@@ -1,3 +1,13 @@
 #!/bin/bash
 
-apt-get update && apt-get install -yq libsndfile1 libpq-dev python3-dev libsndfile1-dev python3-soundfile python-soundfile gcc graphviz graphviz-dev git
+apt-get update && apt-get install -yq \
+    libsndfile1 \
+    libpq-dev \
+    python3-dev \
+    libsndfile1-dev \
+    python3-soundfile \
+    python-soundfile \
+    gcc \
+    graphviz \
+    graphviz-dev \
+    git
