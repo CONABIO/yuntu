94a06abf028b20e0c5de04a5b537f9b6a73d47c2
Everardo <erobredo@conabio.gob.mx>
2021-08-27 16:16:46 -0500

    Add plotting params

--- yuntu/soundscape/utils.py
+++ yuntu/soundscape/utils.py
@@ -109,7 +109,7 @@ def aware_time(strtime, tzone, time_format):
     return dtime
 
 def from_timestamp(timestamp, tzone):
-    dtime = datetime.datetime.fromtimestamp(timestamp)
+    dtime = timestamp.to_pydatetime()
     if dtime.tzinfo is None or dtime.tzinfo.utcoffset(dtime) is None:
         tzobj = pytz.timezone(tzone)
         dtime = tzobj.localize(dtime)
