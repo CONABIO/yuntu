e177566238eb3b9afc76ac2972e3337726ad88d9
Everardo <erobredo@conabio.gob.mx>
2021-09-05 22:24:12 -0500

    numba versioN

--- setup.py
+++ setup.py
@@ -24,7 +24,7 @@ def read_requirements():
 if sys.version[:3] == '3.6':
     install_requires = read_requirements()
 else:
-    install_requires = ['numpy<1.21',
+    install_requires = ['numba>0.50.1',
                         'psycopg2',
                         'pony',
                         'dill',
