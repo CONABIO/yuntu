4e86ca8fc270f75c98f5a9691a47956fcd3ae62f
Everardo <erobredo@conabio.gob.mx>
2021-09-14 17:46:15 -0500

    Datastore fixes

--- yuntu/datastore/base.py
+++ yuntu/datastore/base.py
@@ -18,7 +18,7 @@ class Datastore(ABC):
         self.base_dir = '.'
 
     def get_abspath(self, path):
-        if base_dir is None:
+        if self.base_dir is None:
             return path
         return os.path.join(self.base_dir, path)
 
