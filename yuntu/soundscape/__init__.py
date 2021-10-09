99dbc057fe7385190369f508283dc311e7f7d7ed
Everardo <erobredo@conabio.gob.mx>
2021-10-09 14:01:21 -0500

    Dataframe accesors

--- yuntu/soundscape/__init__.py
+++ yuntu/soundscape/__init__.py
@@ -4,12 +4,9 @@ from . import hashers
 from . import transitions
 from . import pipelines
 
-from yuntu.soundscape.dataframe import SoundscapeAccessor
-
 __all__ = [
     'processors',
     'hashers',
     'transitions',
-    'pipelines',
-    'SoundscapeAccessor'
+    'pipelines'
 ]
